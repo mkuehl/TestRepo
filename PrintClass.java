0564ef5f8996887f7cdf731c57fa5b16ad46551a
mkuehl <maximiliankuehl@web.de>
2015-04-01 17:15:39 +0200

    added setter and print method to PrintClass.java

--- PrintClass.java
+++ PrintClass.java
@@ -1,5 +1,15 @@
 
 public class PrintClass {
 
+	private String textToShow;
+	
 	public PrintClass() {	}
+	
+	public void setTextToShow(String p_textToShow) {
+		textToShow = p_textToShow;
+	}
+	
+	public void printText() {
+		System.out.println(textToShow);
+	}
 }
