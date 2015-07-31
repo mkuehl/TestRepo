1025b4a95e5432493bec3a30bc1a55dc8edf3182
mkuehl <maximiliankuehl@web.de>
2015-07-31 12:31:18 +0200

    added field, getter and setter

--- Printer.java
+++ Printer.java
@@ -6,6 +6,7 @@ import java.sql.Date;
 public class PrintClass {
 
 	private String textToShow;
+	protected short a;
 	
 	public PrintClass() {	}
 	
@@ -15,5 +16,13 @@ public class PrintClass {
 	
 	public void printText() {
 		System.out.println(textToShow);
+	}
+	
+	public void setA(short p_a) {
+		a = p_a;
+	}	
+	
+	public short getA() {
+		return a;
 	}
 }
